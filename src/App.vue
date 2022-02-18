1f9adf9119baed07a1f652574819ff2295e55b6d
mwchoen <cheonmyeongwon@gmail.com>
2022-02-18 16:51:57 +0900

    컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <h1>VUE TODO WITH TYPESCRIPT</h1>
-    <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem"></TodoInput>
+    <header>
+      <h1>VUE TODO WITH TYPESCRIPT</h1>
+    </header>
+    <main>
+      <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem"></TodoInput>
+    </main>
+    <div>
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    </div>
+
+
   </div>
 </template>
 
---
+++ src/components/TotoListItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>item1</li>
+</template>
+
+<script>
+
+export default {
+  name: "TotoListItem"
+};
+</script>
+
+<style scoped>
+
+</style>
