ab25361f7b98d6dd47deef859acb4ba31a334968
mwchoen <cheonmyeongwon@gmail.com>
2022-02-18 15:13:55 +0900

    기본 구조

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input type="text" id="todo-input" :value="item" @input="handleInput"/>
+    <button @click="addTodo">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "TodoInput",
+  props:['item'],
+  methods: {
+    addTodo(){
+      console.log('add');
+    },
+    handleInput(event: any){
+      this.$emit('input', event.target.value);
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
